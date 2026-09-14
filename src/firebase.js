a18e300531533e587e3485d115941a65c93f4ca9
Tanzeel Khan <tanzeel0680@gmail.com>
2026-09-14 23:53:40 +0500

    fixx

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,11 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import React from 'react'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDLjqSXxxIdQNepoHmM4HNjRBEpSR53p2c",
-  authDomain: "investor-bec23.firebaseapp.com",
-  projectId: "investor-bec23",
-  storageBucket: "investor-bec23.firebasestorage.app",
-  messagingSenderId: "661350409211",
-  appId: "1:661350409211:web:688dd34ac7db21cb94a15e"
-};
+const firebase = () => {
+  return (
+    <div>
+      firebase
+    </div>
+  )
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
+export default firebase
